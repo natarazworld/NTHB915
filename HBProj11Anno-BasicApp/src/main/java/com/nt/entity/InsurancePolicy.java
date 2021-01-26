1c04ad291aeb1e2e52684799371398580ab60a22
Nareshit <Nareshit@DESKTOP-IUDAAVL>
2021-01-26 10:08:28 +0530

    HB Anno First App

---
+++ HBProj11Anno-BasicApp/src/main/java/com/nt/entity/InsurancePolicy.java
@@ -0,0 +1,38 @@
+//InsurancePolicy.java
+package com.nt.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="INSURANCE_POLICY")
+/*@Setter
+@Getter
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
+*/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InsurancePolicy implements Serializable {
+	@Id
+	@Column(name="policy_id")
+	private Long  policyId;
+	@Column(name="policy_name",length = 20)
+	private String policyName;
+	@Column(name="company",length = 20)
+	private  String company;
+	@Column(name="tenure")
+	private  Float tenure;
+	
+
+}
