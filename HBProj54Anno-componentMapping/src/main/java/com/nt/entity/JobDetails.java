e8f38280c22cf4d06b0b02ef282ee14681f898aa
Nareshit <Nareshit@DESKTOP-IUDAAVL>
2021-03-23 10:31:01 +0530

    gIT

--- HBProj54Anno-componentMapping/src/main/java/com/nt/entity/JobDetails.java
+++ HBProj54Anno-componentMapping/src/main/java/com/nt/entity/JobDetails.java
@@ -1,4 +1,7 @@
+//JobDetails.java
 package com.nt.entity;
+
+import javax.persistence.Embeddable;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Embeddable
 public class JobDetails {
 	private String desg;
 	private String  company;
--- HBProj54Anno-componentMapping/src/main/java/com/nt/entity/Person.java
+++ HBProj54Anno-componentMapping/src/main/java/com/nt/entity/Person.java
@@ -1,5 +1,10 @@
+//Person.java
 package com.nt.entity;
-
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,10 +15,15 @@ import lombok.RequiredArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @RequiredArgsConstructor
+@Entity
+@Table(name="PERSON_JOB_DETAILS")
 public class Person {
+	@Id
+	@GeneratedValue
   private int pid;
   @NonNull
   private String pname;
   @NonNull
+  @Embedded
   private JobDetails details;
 }
